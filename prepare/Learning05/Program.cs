cdba8028164beec0168f43ebb3e048a47ab10ec6
cristiangsmiranda <cristiangsmiranda@gmail.com>
2024-07-12 01:11:04 -0300

    shapes added

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square a1 = new Square("blue", 2);
+        shapes.Add(a1);
+
+        Rectangle a2 = new Rectangle("pink", 5, 12);
+        shapes.Add(a2);
+
+        Circle a3 = new Circle("red", 7);
+        shapes.Add(a3);
+
+        foreach(Shape a in shapes)
+        {
+            string color = a.GetColor();
+
+            double area = a.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
