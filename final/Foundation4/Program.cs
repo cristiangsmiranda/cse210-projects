ea292a34b825af76ad53f198a3de7b177fe3efb6
cristiangsmiranda <cristiangsmiranda@gmail.com>
2024-07-24 22:01:31 -0300

    Polymorphism with Exercise Tracking added

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,116 @@
 using System;
+using System.Collections.Generic;
+
+public abstract class Activity
+{
+    protected DateTime _date;
+    protected int _minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{_date:dd MMM yyyy} - {GetType().Name} ({_minutes} min) - Distance: {GetDistance():F2} km, Speed: {GetSpeed():F2} kph, Pace: {GetPace():F2} min/km";
+    }
+}
+
+public class Running : Activity
+{
+    private double _distance;
+
+    public Running(DateTime date, int minutes, double distance) : base(date, minutes)
+    {
+        _distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return _distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return _distance / (_minutes / 60.0);
+    }
+
+    public override double GetPace()
+    {
+        return _minutes / _distance;
+    }
+}
+
+public class StationaryBicycle : Activity
+{
+    private double _speed;
+
+    public StationaryBicycle(DateTime date, int minutes, double speed) : base(date, minutes)
+    {
+        _speed = speed;
+    }
+
+    public override double GetDistance()
+    {
+        return (_speed * _minutes) / 60.0;
+    }
+
+    public override double GetSpeed()
+    {
+        return _speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60.0 / _speed;
+    }
+}
+
+public class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(DateTime date, int minutes, int laps) : base(date, minutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return _laps * 50.0 / 1000.0;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / (_minutes / 60.0));
+    }
+
+    public override double GetPace()
+    {
+        return _minutes / GetDistance();
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        var activities = new List<Activity>
+        {
+            new Running(new DateTime(2022, 02, 22), 30, 3.0),
+            new StationaryBicycle(new DateTime(2023, 01, 31), 45, 20.0),
+            new Swimming(new DateTime(2023, 12, 05), 25, 40)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
